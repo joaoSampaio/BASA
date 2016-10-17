06a8cffd3f306db505c5a50828e19413f105e58c
joaoSampaio <joaosampaio30@hotmail.com>
2016-10-17 15:35:34 +0100

    started resumo

---
+++ document/resumo/g.sh
@@ -0,0 +1,4 @@
+gnuplot < gnuplot_demanded_rate.txt
+gnuplot < gnuplot_avg_resp_time.txt
+gnuplot < gnuplot_connection_time.txt
+gnuplot < gnuplot_errosr.txt
---
+++ document/resumo/toPdf.sh
@@ -0,0 +1,7 @@
+/usr/texbin/pdflatex abstract.tex
+/usr/texbin/bibtex abstract
+/usr/texbin/pdflatex abstract.tex
+/usr/texbin/pdflatex abstract.tex
+rm *.log
+rm *.aux
+open abstract.pdf
